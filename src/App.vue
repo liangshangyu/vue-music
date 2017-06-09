98498a200cda88260d66b6bcdb5b8f5422880f0c
liangshangyu <2460641129@qq.com>
2017-06-09 14:02:05 +0800

    xiaoxiugai

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ export default {
     this.$store.commit('findDOM', {name: 'audio', dom: this.$refs.audio});
     this.$refs.audio.addEventListener('ended', () => { this.next(); });
     this.$refs.audio.addEventListener('error', () => { this.next(); });
-    console.log("%c Powered by Zhaohui - microzz.com","background-image:-webkit-gradient( linear, left top,right top, color-stop(0, #00a419),color-stop(0.15, #f44336), color-stop(0.29, #ff4300),color-stop(0.3, #AA00FF),color-stop(0.4, #8BC34A), color-stop(0.45, #607D8B),color-stop(0.6, #4096EE), color-stop(0.75, #D50000),color-stop(0.9, #4096EE), color-stop(1, #FF1A00));color:transparent;-webkit-background-clip:text;font-size:13px;");
+    console.log("%c Powered by Lsy ","background-image:-webkit-gradient( linear, left top,right top, color-stop(0, #00a419),color-stop(0.15, #f44336), color-stop(0.29, #ff4300),color-stop(0.3, #AA00FF),color-stop(0.4, #8BC34A), color-stop(0.45, #607D8B),color-stop(0.6, #4096EE), color-stop(0.75, #D50000),color-stop(0.9, #4096EE), color-stop(1, #FF1A00));color:transparent;-webkit-background-clip:text;font-size:13px;");
   },
   computed: {
     audio() {
